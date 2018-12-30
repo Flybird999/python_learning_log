d5f727c939073ebb9fc325bc47ea944ce7874c21
Flybird999 <2944674230@qq.com>
2018-12-30 23:02:41 +0800

    转战菜鸟教程--教材变化
    在菜鸟教程网站学习。

--- 学习资料/pythonl练习代码/python练习代码_1.py
+++ 学习资料/pythonl练习代码/python练习代码_1.py
@@ -1,5 +1,10 @@
 # 彩蛋
 # import this 
+
 # print ("hello the world !") #国际惯例，哈哈哈
-who_are_you = input ("who are you ?\n")
-print ("you are %s" % who_are_you) 
+
+''' who_are_you = input ("who are you ?\n")
+print ("you are %s" % who_are_you) '''
+
+#一个很严重的问题，python的版本问题，python2.X和python3.x的完全不同区别。
+#本小白要学习python3.x，所以笨办法学python这个教材不合适，转战，菜鸟教程。
