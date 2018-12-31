f4bf030e59e4a8f18b8b028eea83d3922d32485f
Flybird999 <2944674230@qq.com>
2018-12-31 12:52:24 +0800

    菜鸟教程-学习完
    下一步，实战学习，爬虫

--- 学习资料/pythonl练习代码/python练习代码_菜鸟教程_1.py
+++ 学习资料/pythonl练习代码/python练习代码_菜鸟教程_1.py
@@ -4,9 +4,15 @@
 '''import keyword
 print(keyword.kwlist)'''
 
-import sys
+#系统命令行参数
+'''import sys
 
 print("命令行参数如下：")
 for i in sys.argv:
     print(i)
-print('\n\n Python 路径为：',sys.path,'\n')
+print('\n\n Python 路径为：',sys.path,'\n') 
+print (__name__)'''
+
+#命令行参数的保存
+import sys
+print(sys.argv)
